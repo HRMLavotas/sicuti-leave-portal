9cb48be66d81b540766de1a4cf189456b18a0be4
HRMlavotas <hrmlavotas@gmail.com>
2026-06-26 15:58:51 +0700

    Update migration: Add leave_period column to leave_proposal_items as well

--- add_employee_rank_to_leave_proposal_items.sql
+++ add_employee_rank_to_leave_proposal_items.sql
@@ -1,8 +1,10 @@
--- Add employee_rank column to leave_proposal_items table
--- This column stores the employee's rank at the time of the proposal
+-- Add employee_rank and leave_period columns to leave_proposal_items table
+-- These columns store the employee's rank and leave period at the time of the proposal
 
 ALTER TABLE leave_proposal_items 
-ADD COLUMN IF NOT EXISTS employee_rank TEXT;
+ADD COLUMN IF NOT EXISTS employee_rank TEXT,
+ADD COLUMN IF NOT EXISTS leave_period INTEGER;
 
--- Add comment
+-- Add comments
 COMMENT ON COLUMN leave_proposal_items.employee_rank IS 'The employee''s rank at the time of proposal submission';
+COMMENT ON COLUMN leave_proposal_items.leave_period IS 'The leave period year (e.g., 2024) for which the leave is requested';
